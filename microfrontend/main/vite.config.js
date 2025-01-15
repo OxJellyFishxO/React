9a2d05233c1e7dfee673dc4d1a214562d185dc24
Jovany Lugo Moreno <jovanylm23@gmail.com>
2025-01-15 14:02:50 -0600

    Main de Microfrontend

--- microfrontend/main/vite.config.js
+++ microfrontend/main/vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// Importacion del paquete vite-plugin-federation
+import federation from '@originjs/vite-plugin-federation'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // Importamos los Micro Frontends 'Imagen' y 'Web'
+    federation({
+      name: "main",
+      //Aplicaciones remotas que nuestra aplicacion puede consumir
+      remotes: {
+        imagen: "http://localhost:4173/assets/remoteEntry.js",
+        web: "http://localhost:4174/assets/remoteEntry.js",
+      },
+      shared:["react"],
+    }),
+  ],
+
+  build: {
+    // Configuramos la compilación de producción
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false
+  }
 })
